1a0ae000f4080bcf415aa9c0cb132c4250c4d4b5
XpertDude <braodby@gmail.com>
2025-07-22 13:06:26 +0100

    finial

---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+// constants/index.ts
+
+export const PROPERTY_LISTING_SAMPLE = [
+  {
+    image: '/assets/house1.jpg',
+    name: 'Cozy Cottage',
+    price: 120,
+    rating: 4.8,
+  },
+  {
+    image: '/assets/villa.jpg',
+    name: 'Luxury Villa',
+    price: 250,
+    rating: 4.9,
+  },
+];
+
+export const FILTERS = [
+  'Top Villa',
+  'Self Checkin',
+  'Countryside',
+  'Beachfront',
+];
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,9 +1,13 @@
-export interface CardProps {
-    title: string;
-    styles: string;
-}
+// interfaces/index.ts
 
 export interface ButtonProps {
     title: string;
     styles: string;
+}
+
+export interface CardProps {
+    image: string;
+    name: string;
+    price: number;
+    rating: number;
 }
